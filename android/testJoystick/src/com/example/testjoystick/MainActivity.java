479e566767e2e4e17f0bc110c261f95d6987be07
Scott Godbold <scgodbol@umich.edu>
2014-03-12 22:28:20 -0400

    Masked the stupid joystick hovering with a large transparent button....FML

--- android/testJoystick/src/com/example/testjoystick/MainActivity.java
+++ android/testJoystick/src/com/example/testjoystick/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().hide();
         
         reset = true;
         previousEvent = 0;
