9afa78f786ce36a37350dd8248a9214278f04a81
kaewang <kaewang@umich.edu>
2014-03-24 21:15:34 -0400

    compressed tracks, added tracks to AllTracks

--- android/GraceBand/src/com/eecs481/graceband/AllTracks.java
+++ android/GraceBand/src/com/eecs481/graceband/AllTracks.java
@@ -12,11 +12,36 @@ public class AllTracks{
 	public AllTracks(){
 		tracks = new ArrayList<Track>();
 		//initialize all available tracks
-		tracks.add(new Track(R.raw.tambourine, "Tambourine", R.drawable.selectorsnare_trackmenu, R.drawable.selectorsnare));
-		tracks.add(new Track(R.raw.piano, "Piano", R.drawable.selectorpiano_trackmenu, R.drawable.selectorpiano));
-		tracks.add(new Track(R.raw.bongo, "Bongos", R.drawable.selectorbass_trackmenu, R.drawable.selectorbass));
-		tracks.add(new Track(R.raw.vocal, "Vocals", R.drawable.selectorvocals_trackmenu, R.drawable.selectorvocals));
-		tracks.add(new Track(R.raw.guitar, "Guitar", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+//		tracks.add(new Track(R.raw.tambourine, "Tambourine", R.drawable.selectorsnare_trackmenu, R.drawable.selectorsnare));
+//		tracks.add(new Track(R.raw.piano, "Piano", R.drawable.selectorpiano_trackmenu, R.drawable.selectorpiano));
+//		tracks.add(new Track(R.raw.bongo, "Bongos", R.drawable.selectorbass_trackmenu, R.drawable.selectorbass));
+//		tracks.add(new Track(R.raw.vocal, "Vocals", R.drawable.selectorvocals_trackmenu, R.drawable.selectorvocals));
+//		tracks.add(new Track(R.raw.guitar, "Guitar", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.bass_1, "Bass 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.bass_2, "Bass 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.basschord_1, "Bass Chords 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.basschord_2, "Bass Chords 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.basschord_3, "Bass Chords 3", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.harpsich_1, "Harpsichord 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.harpsich_2, "Harpsichord 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.hat_1, "Hat 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.hat_2, "Hat 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.hat_3, "Hat 3", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.kick_1, "Kick 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.kick_2, "Kick 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.kickclap_1, "Kick+Clap 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.kickclap_2, "Kick+Clap 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.kicksnare_1, "Kick+Snare 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.kicksnare_2, "Kick+Snare 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.piano_1, "Piano 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.piano_2, "Piano 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.piano_3, "Piano 3", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.piano_4, "Piano 4", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.piano_5, "Piano 5", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.piano_6, "Piano 6", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.sitar_1, "Sitar 1", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		tracks.add(new Track(R.raw.sitar_2, "Sitar 2", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
+		
 	}
 	
 	public class TrackNotFoundException extends Exception {
