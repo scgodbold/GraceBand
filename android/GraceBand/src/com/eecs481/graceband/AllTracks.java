9cde7d85f007ef273b0ecf8f5443bea928724279
Scott Godbold <scgodbol@umich.edu>
2014-04-21 19:45:28 -0400

    Added new tracks whoop

--- android/GraceBand/src/com/eecs481/graceband/AllTracks.java
+++ android/GraceBand/src/com/eecs481/graceband/AllTracks.java
@@ -19,23 +19,25 @@ public class AllTracks{
 //		tracks.add(new Track(R.raw.guitar, "Guitar", R.drawable.selectorbeatbox_trackmenu, R.drawable.selectorbeatbox));
 		tracks.add(new Track(R.raw.bass_1, "Bass 1", R.drawable.selector_bass_green, R.drawable.selector_bass_green_remove,Category.BASS));
 		tracks.add(new Track(R.raw.bass_2, "Bass 2", R.drawable.selector_bass_pink, R.drawable.selector_bass_pink_remove,Category.BASS));
+		tracks.add(new Track(R.raw.bass_3, "Bass 3", R.drawable.selector_bass_blue, R.drawable.selector_bass_blue_remove, Category.BASS));
 		
-		tracks.add(new Track(R.raw.basschord_1, "Bass Chords 1", R.drawable.selector_cord_purple, R.drawable.selector_cord_purple_remove,Category.CHORD));
-		tracks.add(new Track(R.raw.basschord_2, "Bass Chords 2", R.drawable.selector_cord_pink, R.drawable.selector_cord_pink_remove,Category.CHORD));
-		tracks.add(new Track(R.raw.basschord_3, "Bass Chords 3", R.drawable.selector_cord_blue, R.drawable.selector_cord_blue_remove,Category.CHORD));
-		tracks.add(new Track(R.raw.harpsich_1, "Harpsichord 1", R.drawable.selector_cord_red, R.drawable.selector_cord_red_remove,Category.CHORD));
-		tracks.add(new Track(R.raw.harpsich_2, "Harpsichord 2", R.drawable.selector_cord_green, R.drawable.selector_cord_green_remove,Category.CHORD));
+//		tracks.add(new Track(R.raw.basschord_1, "Bass Chords 1", R.drawable.selector_cord_purple, R.drawable.selector_cord_purple_remove,Category.CHORD));
+//		tracks.add(new Track(R.raw.basschord_2, "Bass Chords 2", R.drawable.selector_cord_pink, R.drawable.selector_cord_pink_remove,Category.CHORD));
+//		tracks.add(new Track(R.raw.basschord_3, "Bass Chords 3", R.drawable.selector_cord_blue, R.drawable.selector_cord_blue_remove,Category.CHORD));
+		tracks.add(new Track(R.raw.harpsich_1, "Harpsichord", R.drawable.selector_cord_red, R.drawable.selector_cord_red_remove,Category.CHORD));
+		tracks.add(new Track(R.raw.orch_1, "Orchestra", R.drawable.selector_cord_green, R.drawable.selector_cord_green_remove,Category.CHORD));
 		
 		tracks.add(new Track(R.raw.hat_1, "Hat 1", R.drawable.selector_cymbal_blue, R.drawable.selector_cymbal_blue_remove,Category.CYMBAL));
 		tracks.add(new Track(R.raw.hat_2, "Hat 2", R.drawable.selector_cymbal_pink, R.drawable.selector_cymbal_pink_remove,Category.CYMBAL));
 		tracks.add(new Track(R.raw.hat_3, "Hat 3", R.drawable.selector_cymbal_green, R.drawable.selector_cymbal_green_remove,Category.CYMBAL));
+		tracks.add(new Track(R.raw.bell_1, "Bell", R.drawable.selector_cymbal_red, R.drawable.selector_cymbal_red_remove, Category.CYMBAL));
 		
 		tracks.add(new Track(R.raw.kick_1, "Kick 1", R.drawable.selector_drum_orange, R.drawable.selector_drum_orange_remove,Category.DRUM));
 		tracks.add(new Track(R.raw.kick_2, "Kick 2", R.drawable.selector_drum_red, R.drawable.selector_drum_red_remove,Category.DRUM));
+		tracks.add(new Track(R.raw.kick_3, "Kick 3", R.drawable.selector_drum_blue, R.drawable.selector_drum_blue_remove,Category.DRUM));
 		tracks.add(new Track(R.raw.kickclap_1, "Kick+Clap 1", R.drawable.selector_drum_purple, R.drawable.selector_drum_purple_remove,Category.DRUM));
 		tracks.add(new Track(R.raw.kickclap_2, "Kick+Clap 2", R.drawable.selector_drum_pink, R.drawable.selector_drum_pink_remove,Category.DRUM));
 		tracks.add(new Track(R.raw.kicksnare_1, "Kick+Snare 1", R.drawable.selector_drum_green, R.drawable.selector_drum_green_remove,Category.DRUM));
-		tracks.add(new Track(R.raw.kicksnare_2, "Kick+Snare 2", R.drawable.selector_drum_blue, R.drawable.selector_drum_blue_remove,Category.DRUM));
 		
 		tracks.add(new Track(R.raw.piano_1, "Piano 1", R.drawable.selector_piano_red, R.drawable.selector_piano_red_remove,Category.PIANO));
 		tracks.add(new Track(R.raw.piano_2, "Piano 2", R.drawable.selector_piano_blue, R.drawable.selector_piano_blue_remove,Category.PIANO));
@@ -46,6 +48,11 @@ public class AllTracks{
 		
 		tracks.add(new Track(R.raw.sitar_1, "Sitar 1", R.drawable.selector_sitar_pink, R.drawable.selector_sitar_pink_remove,Category.SITAR));
 		tracks.add(new Track(R.raw.sitar_2, "Sitar 2", R.drawable.selector_sitar_green, R.drawable.selector_sitar_green_remove,Category.SITAR));
+		
+		tracks.add(new Track(R.raw.epiano_1, "E-Piano 1", R.drawable.selector_question_orange, R.drawable.selector_question_orange_remove, Category.MISC));
+		tracks.add(new Track(R.raw.epiano_2, "E-Piano 2", R.drawable.selector_question_red, R.drawable.selector_question_red_remove, Category.MISC));
+		tracks.add(new Track(R.raw.zen_1, "Zen 1", R.drawable.selector_question_purple, R.drawable.selector_question_purple_remove, Category.MISC));
+		tracks.add(new Track(R.raw.zen_2, "Zen 1", R.drawable.selector_question_blue, R.drawable.selector_question_blue_remove, Category.MISC));
 		
 	}
 	
--- android/GraceBand/src/com/eecs481/graceband/Category.java
+++ android/GraceBand/src/com/eecs481/graceband/Category.java
@@ -2,9 +2,10 @@ package com.eecs481.graceband;
 
 public enum Category {
 	DRUM,
+	MISC,
 	BASS,
 	CYMBAL,
+	PIANO,
 	CHORD,
-	SITAR,
-	PIANO
+	SITAR
 }
